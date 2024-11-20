4f883d18c809f164cf5534e63a8e3f0fedd06f43
YoungjunYang <dididudud@naver.com>
2024-11-20 12:38:43 +0900

    del

--- class01/homework/yangyeongjun/python2.py
+++
@@ -1,9 +0,0 @@
-n = int(input("정수 n을 입력하세요: "))
-
-
-
-for i in range(0,n):
-    print("")
-    for j in range(0,n):
-        print((j+1)+n*i, end='')
-print("")
--- class01/homework/yangyeongjun/python3.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-
-n = int(input("정수 n을 입력하세요: "))
-
-matrix = np.arange(1, n*n + 1).reshape(-1)
-print(matrix)
